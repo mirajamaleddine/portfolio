422031d456bf0253c2edef363023abc08d422597
Mira <mirajamaleddine13@gmail.com>
2026-02-11 21:22:48 +0200

    deploy astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite'
 
-import react from '@astrojs/react';
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://mirajamaleddine.github.io',
+  base: '/portfolio/', // ONLY if project repo
+  output: 'static',
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
-  integrations: [react()]
-});
+  integrations: [react()],
+})
