585feec8839c153a46c94e8669ea1976f6bddf83
Mira <mirajamaleddine13@gmail.com>
2026-02-11 21:38:13 +0200

    fix base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import react from '@astrojs/react'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mirajamaleddine.github.io',
-  base: '/portfolio/', // ONLY if project repo
+  base: '/', // ONLY if project repo
   output: 'static',
   vite: {
     plugins: [tailwindcss()],
